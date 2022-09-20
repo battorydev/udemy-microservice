51cac4e361092e5d54e07c5ecc7daef681158011
battorydev <battorydev@users.noreply.github.com>
2022-09-21 02:17:01 +0700

    Create twiiter-to-kafka submodule

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter_to_kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.twitter_to_kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}

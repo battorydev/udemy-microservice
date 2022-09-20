2056a3a65d287bbf76220beb3fd6c3e1620f26f5
battorydev <battorydev@users.noreply.github.com>
2022-09-21 02:24:36 +0700

    Add Main Application with log

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter_to_kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter_to_kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,12 +1,22 @@
 package com.microservices.demo.twitter_to_kafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("Hello");
     }
 }
